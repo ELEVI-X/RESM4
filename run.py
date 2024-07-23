861fe9ff3b6cd9911ebc974f3038e358a53733f8
ELEVI-X <kalimeelan977@gmail.com>
2024-07-23 22:38:21 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os, platform
 os.system('cls' if 'win' in sys.platform.lower() else 'clear')
-print('Checking Updates');os.system('git pull')
+print('Checking Updates');os.system('git pull -q')
 bit = platform.architecture()[0]
 if bit == '32bit':
     print(f'32Bit coming soon ');exit()
