7096deb22b81a412958f6609f75edbf833b72903
AKA MAX <kalimeelan977@gmail.com>
2024-08-01 02:28:10 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 import os,platform,sys
 os.system('cls' if 'win' in sys.platform.lower() else 'clear')
 print('Checking Updates');os.system('git pull -q')
-bit = platform.architecture()[0]
-if bit == '32bit':
-    print(f'32Bit coming soon ');exit()
-elif bit == '64bit':
-    import RESM64
-else: print("Tool not supported");exit()
+print(f' This tool is removed \n you can use new versio ')
+print(f''' Command 
+
+''')
