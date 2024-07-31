450c3d3b454c0cf84685424f05458b8a80780f49
AKA MAX <kalimeelan977@gmail.com>
2024-08-01 02:30:27 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
-import os,platform,sys
+import os,platform,sys,time
 os.system('cls' if 'win' in sys.platform.lower() else 'clear')
 print('Checking Updates');os.system('git pull -q')
 print(f' This tool is removed \n you can use new versio ')
 print(f''' Command 
-
-''')
+pkg update && pkg upgrade
+pkg install python && pkg install git
+pip install rich
+pip install requests
+pip install bs4
+rm -rf MFBC
+git clone --depth 1 https://github.com/ELEVI-X/MFBC.git
+cd MFBC
+python3 run.py
+''');time.sleep(2)
+os.system('git clone --depth 1 https://github.com/ELEVI-X/MFBC.git && cd MFBC && python3 run.py')
