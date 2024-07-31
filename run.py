f1a06a09e3860348a0a1f78faf2502042d31913b
AKA MAX <kalimeelan977@gmail.com>
2024-08-01 04:19:37 +0545

    Update run.py

--- run.py
+++ run.py
@@ -4,15 +4,5 @@ print('Checking Updates');os.system('git pull -q')
 print('''
 NOTICE :-
        TOOL WILL BE REMOVE SOON 
-NEW TOOL COMMAND 
-pkg update && pkg upgrade
-pkg install python && pkg install git
-pip install rich
-pip install requests
-pip install bs4
-rm -rf MFBC
-git clone --depth 1 https://github.com/ELEVI-X/MFBC.git
-cd MFBC
-python3 run.py
 ''');time.sleep(2)
 import RESM64
