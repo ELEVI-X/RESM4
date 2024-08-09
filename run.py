5d67e63420c895b59a1848e7f3cff7f6e10d4853
AKA MAX <kalimeelan977@gmail.com>
2024-08-09 13:39:16 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 import os,platform,sys,time
 os.system('cls' if 'win' in sys.platform.lower() else 'clear')
 print('Checking Updates');os.system('git pull -q')
-print('''
-NOTICE :-
-       TOOL WILL BE REMOVE SOON 
-''');time.sleep(2)
 import RESM64
